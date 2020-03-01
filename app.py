909e2bb61e9b0fb4c5c1355ca8124e3dcbfe34f4
kiki <kaylasharky@hotmail.com>
2020-02-29 19:59:54 -0500

    Understood templates

--- app.py
+++ app.py
@@ -1,12 +1,13 @@
-from flask import Flask
+from flask import Flask, url_for, render_template
 
 app = Flask(__name__)
 
 
 @app.route('/')
+@app.route('/home')
 def hello_world():
-    return 'Hello World!'
+    return render_template('home.html')
 
 
 if __name__ == '__main__':
-    app.run()
+    app.run(debug=True)
